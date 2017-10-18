d022a6b816aa9a6d70079f249e7bf79af91c80f0
Radoslaw Fabisiak <radoslawfabisiak@gmail.com>
2017-10-18 12:10:06 +0200

    Initial commit.

---
+++ script.js
@@ -0,0 +1,28 @@
+if (String.prototype.repeat === undefined){
+	
+	console.log("metoda repeat niezdefiniowana przez przeglądarkę.");
+	
+	String.prototype.repeat = function (i){
+		
+		var value = "";
+		
+		for (var j=0; j<i; j++){
+			
+			value += this;
+			
+		}
+		
+		return value;
+	};
+		
+	} else {
+		
+		console.log("metoda repeat zdefiniowana przez przeglądarkę.");	
+	
+}
+
+var tytul = document.querySelector("#tytul2");
+
+var cos = "cos ";
+
+tytul.innerHTML = cos.repeat(3);
